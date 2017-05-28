63b45a82df1de62f05a8fd485fb87becaa623a6f
poi5305 <poi5305@gmail.com>
2017-05-29 00:18:53 +0800

    add serverless and do some api

---
+++ handler.js
@@ -0,0 +1,158 @@
+const AWS = require('aws-sdk');
+const fs = require('fs');
+const _ = require('lodash');
+const co = require('co');
+const zlib = require('zlib');
+const Epub = require('epub-gen');
+const fp = require('./function_pipe.js');
+const bookTable = require('./db_books.js');
+
+const novelBucket = 'ck101-novels';
+const folder = 'novels';
+
+function doResponse(callback, statusCode, body) {
+  const response = {
+    statusCode,
+    body: JSON.stringify(body),
+  };
+  callback(null, response);
+}
+
+function doResponseUTF8(callback, statusCode, filename, buffer) {
+  const response = {
+    statusCode,
+    body: buffer.toString('utf8'),
+    headers: {
+      'Content-Type': 'application/force-download',
+      'Content-Transfer-Encoding': 'Binary',
+      'Content-Disposition': `filename*=UTF-8''${encodeURI(filename)}`,
+    },
+    // isBase64Encoded: true,
+  };
+  callback(null, response);
+}
+
+function doResponseBASE64(callback, statusCode, filename, buffer) {
+  const response = {
+    statusCode,
+    body: buffer.toString('base64'),
+    headers: {
+      'Content-Type': 'application/octet-stream',
+      'Content-Transfer-Encoding': 'Binary',
+      'Content-Disposition': `filename*=UTF-8''${encodeURI(filename)}`,
+    },
+    isBase64Encoded: true,
+  };
+  callback(null, response);
+}
+
+function getBookHeaderText(book) {
+  let bookHeader = `書名: ${book.title}\n`;
+  bookHeader += `分類: ${book.classify}\n`;
+  bookHeader += '程式作者: Andy\n';
+  bookHeader += `總章數: ${book.updatedPost}\n`;
+  bookHeader += `總頁數: ${book.updatedPage}\n\n`;
+  return bookHeader;
+}
+
+module.exports.getBooks = (event, context, callback) => {
+  const limit = 10;
+  const lastBookId = _.toNumber(_.get(event, 'queryStringParameters.lastBookId', 0));
+  let lastKey;
+  if (lastBookId !== 0) {
+    lastKey = { bookId: lastBookId };
+  }
+  fp
+  .pipe(fp.bind(bookTable.scan, limit, lastKey))
+  .pipe(fp.bind(doResponse, callback, 200))
+  .catch(fp.bind(doResponse, callback, 400))
+  ;
+};
+
+module.exports.getBooksInfo = (event, context, callback) => {
+  const bookId = _.toNumber(_.get(event, 'pathParameters.bookId', 0));
+  fp
+  .pipe(fp.bind(bookTable.getItem, bookId))
+  .pipe(fp.bind(doResponse, callback, 200))
+  .catch(fp.bind(doResponse, callback, 400))
+  ;
+};
+
+module.exports.getBooksTXT = (event, context, callback) => {
+  const s3 = new AWS.S3();
+  const bookId = _.toNumber(_.get(event, 'pathParameters.bookId', 0));
+  fp
+  .pipe(fp.bind(bookTable.getItem, bookId), null, [1, 2])
+  .pipe(book => `${book.title}.txt`, null, 3)
+  .pipe(book => co(function* () {
+    const maxPage = _.get(book, 'updatedPage', 0);
+
+    let buffer = new Buffer(getBookHeaderText(book));
+    for (let page = 1; page <= 1; page += 1) {
+      const key = `${folder}/${bookId}/${page}.gzip`;
+      yield fp
+      .pipe(() => s3.getObject({ Bucket: novelBucket, Key: key }).promise())
+      .pipe(obj => obj.Body)
+      .pipe(zlib.gunzipSync)
+      .pipe(txtBuf => new Promise((resolve) => {
+        buffer = Buffer.concat([buffer, txtBuf]);
+        resolve(buffer);
+      }))
+      .promise;
+    }
+    return buffer;
+  }))
+  .pipe(b => b)
+  .pipe(fp.bind(doResponseUTF8, callback, 200))
+  .catch(fp.bind(doResponse, callback, 400))
+  ;
+};
+
+module.exports.getBooksEBook = (event, context, callback) => {
+  const s3 = new AWS.S3();
+  const bookId = _.toNumber(_.get(event, 'pathParameters.bookId', 0));
+  fp
+  .pipe(fp.bind(bookTable.getItem, bookId), null, [1, 2])
+  .pipe(book => `${book.title}.epub`, null, 3)
+  .pipe(book => co(function* () {
+    const maxPage = _.get(book, 'updatedPage', 0);
+    const header = _.replace(getBookHeaderText(book), /\n/g, '<br />');
+    const epubOption = {
+      title: book.title,
+      author: 'aws-novel (程式作者 Andy)',
+      publisher: 'Andy',
+      content: [{
+        title: '資訊',
+        data: `<p>${header}</p>`,
+      }],
+      tempDir: '/tmp/',
+    };
+    // const imageURL = _.get(book, 'imageURL', undefined);
+    // if (!_.isUndefined) {
+    //   epubOption.cover = imageURL;
+    // }
+
+    for (let page = 1; page <= 1; page += 1) {
+      const key = `${folder}/${bookId}/${page}.gzip`;
+      yield fp
+      .pipe(() => s3.getObject({ Bucket: novelBucket, Key: key }).promise())
+      .pipe(obj => obj.Body)
+      .pipe(zlib.gunzipSync)
+      .pipe(txtBuf => new Promise((resolve) => {
+        const content = _.replace(txtBuf.toString('utf8'), /\n/g, '<br />');
+        epubOption.content.push({
+          title: `Page: ${page}`,
+          data: `<p>${content}</p>`,
+        });
+        resolve();
+      }))
+      .promise;
+    }
+    return new Epub(epubOption, '/tmp/tmp.epub').promise;
+  }))
+  .pipe(() => fs.readFileSync('/tmp/tmp.epub'))
+  .pipe(fp.bind(doResponseBASE64, callback, 200))
+  .catch(fp.bind(doResponse, callback, 400))
+  ;
+};
+
---
+++ test/handler.test.js
@@ -0,0 +1,66 @@
+
+'use strict';
+
+const should = require('chai').should();
+const expect = require('chai').expect;
+const fp = require('../function_pipe.js');
+
+const handler = require('../handler.js');
+
+const apiWapper = (apiFunc, event) => new Promise((resolve) => {
+  apiFunc(event, {}, (_, response) => {
+    resolve(response);
+  });
+});
+
+describe('Handler', () => {
+  const event = {};
+
+  // it('should get book list', (done) => {
+  //   fp
+  //   .pipe(fp.bind(apiWapper, handler.getBooks, event))
+  //   .pipe(response => JSON.parse(response.body))
+  //   .pipe(body => body.LastEvaluatedKey.bookId)
+  //   .pipe((lastKey) => {
+  //     event.queryStringParameters = { lastBookId: lastKey };
+  //     return event;
+  //   })
+  //   .pipe(fp.bind(apiWapper, handler.getBooks))
+  //   .pipe((response) => {
+  //     expect(response.statusCode).to.equal(200);
+  //     done();
+  //   });
+  // });
+
+  // it('should get book info', (done) => {
+  //   event.pathParameters = { bookId: '3729819' };
+  //   fp
+  //   .pipe(fp.bind(apiWapper, handler.getBooksInfo, event))
+  //   .pipe((response) => {
+  //     expect(response.statusCode).to.equal(200);
+  //     done();
+  //   });
+  // });
+
+  it('should get book txt', (done) => {
+    event.pathParameters = { bookId: '3729819' };
+    fp
+    .pipe(fp.bind(apiWapper, handler.getBooksTXT, event))
+    .pipe((response) => {
+      console.log(response);
+      // expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+  return;
+  it('should get book txt', (done) => {
+    event.pathParameters = { bookId: '3729819' };
+    fp
+    .pipe(fp.bind(apiWapper, handler.getBooksEBook, event))
+    .pipe((response) => {
+      console.log(response);
+      // expect(response.statusCode).to.equal(200);
+      done();
+    });
+  }).timeout(10000);
+});
